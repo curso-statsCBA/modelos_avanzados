9a6a88489f687abb94260f3c49790ed9c157122a
santiagombv <santiagombv@gmail.com>
2026-07-30 18:45:51 -0300

    update tp5

--- TP5/practico05.R
+++ TP5/practico05.R
@@ -1,5 +1,11 @@
+#────────#
+# Caso 1 #
+#────────#
+
 library(nlme)
-dat <- read.table("tempcorr.txt", header = TRUE)
+library(performance)
+
+dat <- read.table("TP5/tempcorr.txt", header = TRUE)
 plot(abund ~ year, data = dat)
 plot(abund ~ rain, data = dat)
 
@@ -8,6 +14,7 @@ m0 <- gls(abund ~ rain + year, na.action = na.omit, data = dat)
 summary(m0)
 AIC(m0)
 plot(m0)
+check_model(m0)
 acf(m0$residuals)
 
 # Modelo con autocorrelación de simetría compuesta
@@ -22,68 +29,100 @@ m2 <- gls(abund ~ rain + year, na.action = na.omit, data = dat,
 summary(m2)
 AIC(m2)
 
-?gls
+AIC(m0, m1, m2)
 
-###Ejemplo espacial###
-dat <- read.table("Boreality.txt", header = TRUE)
-head(dat)
 
-## modelo lineal (incorrecto)
-fit1 <- lm (Bor ~ Wet, data = dat)
-summary(fit1)
+#────────#
+# Caso 2 #
+#────────#
 
-## visualización de los residuos en el espacio
-## 1) Bubble plots
-library(gstat)
 library(sp)
-E <- rstandard(fit1)
-spatial <- data.frame(E, dat$x, dat$y)
-coordinates(spatial) <- c("dat.x", "dat.y")
-bubble(spatial, "E")
-
-## 2) Variograma
-## el eje y indica cuan diferentes son las obs.
-Vario1 =variogram(E ~ 1, spatial)#no corre
-plot(Vario1)
-
-?Variogram
-
-## Agregar correlaciones al modelo
+library(gstat)
 library(nlme)
 
-m1 <- gls(Bor ~ Wet, data = dat) # sin correlación
+data(meuse)
+meuse_df <- meuse # data frame
+meuse_sp <- meuse # spatial object
+coordinates(meuse_sp) <- ~x + y
 
-m2 <- gls(Bor ~ Wet, data = dat, correlation = 
-            corSpher(form =~ x+y, nugget = TRUE))
+# zinc tiene sesgo a la derecha
+meuse_df$logzinc <- log(meuse_df$zinc)
+meuse_sp$logzinc <- log(meuse_sp$zinc)
 
-m3 <- gls(Bor ~ Wet, data = dat, correlation = 
-            corLin(form =~ x+y, nugget = TRUE))
+# Bubble plot
+meuse_sp$scaled_logzinc <- log(meuse_sp$zinc) - mean(log(meuse_sp$zinc))
+bubble(
+  meuse_sp, "scaled_logzinc",
+  main = "log(Zinc) concentration - Meuse floodplain",
+  col = c("steelblue", "orangered")
+)
 
-m4 <- gls(Bor ~ Wet, data = dat, correlation = 
-            corRatio(form =~ x+y, nugget = TRUE))
+# Comparar con distancia al río
+bubble(
+  meuse_sp, "dist",
+  main = "Distance to river",
+  col = c("darkgreen", "orange")
+)
 
-m5 <- gls(Bor ~ Wet, data = dat, correlation = 
-            corGaus(form =~ x+y, nugget = TRUE))
+# Variograma de la variable respuesta
+vgm_raw <- variogram(logzinc ~ 1, meuse_sp)
+plot(vgm_raw, type = "b")
 
-m6 <- gls(Bor ~ Wet, data = dat, correlation = 
-            corExp(form =~ x+y, nugget = TRUE))
+# Variograma de los residuos zinc ~ distancia al río
+vgm_resid <- variogram(logzinc ~ dist, meuse_sp)
+plot(vgm_resid, type = "b")
 
-AIC(m1, m2, m3, m4, m5, m6)
+vgm_cloud <- variogram(logzinc ~ dist, meuse_sp, cloud = TRUE)
+plot(vgm_cloud)
 
-## Examinando nuevamente los variogramas
-var1 <- Variogram(m1, form =~x+y, robust = TRUE, 
-                  resType = "normalized")
-plot(var1)
+show.vgms() # modelos posibles
 
-var1 <- Variogram(m1, form =~x+y, robust = TRUE, 
-                  resType = "normalized", maxDist = 2000)
-plot(var1)
+# modelos con diferente estructura espacial
+m0 <- gls(logzinc ~ dist, data = meuse_df)
 
+m_exp <- gls(
+  logzinc ~ dist, data = meuse_df,
+  correlation = corExp(form = ~x + y, nugget = TRUE)
+)
 
-var6 <- Variogram(m6, form =~x+y, robust = TRUE, 
-                  resType = "normalized", maxDist = 5000)
-plot(var6)
+m_gaus <- gls(
+  logzinc ~ dist, data = meuse_df,
+  correlation = corGaus(form = ~x + y, nugget = TRUE)
+)
 
-## modelo final
-summary(m6)
-plot(m6)
+m_sph <- gls(
+  logzinc ~ dist, data = meuse_df,
+  correlation = corSpher(form = ~x + y, nugget = TRUE)
+)
+
+m_ratio <- gls(
+  logzinc ~ dist, data = meuse_df,
+  correlation = corRatio(form = ~x + y, nugget = TRUE)
+)
+
+m_lin <- gls(
+  logzinc ~ dist, data = meuse_df,
+  correlation = corLin(form = ~x + y, nugget = TRUE)
+)
+
+# Comparación de modelos
+AIC(m0, m_exp, m_gaus, m_sph, m_ratio, m_lin)
+
+# modelo ganador
+summary(m_gaus)
+
+# Comparación de los variogramas
+var0 <- Variogram(
+  m0, form = ~ x + y, robust = TRUE, resType = "normalized"
+)
+plot(var0)
+
+var_gaus <- Variogram(
+  m_gaus, form = ~ x + y, robust = TRUE, resType = "normalized"
+)
+plot(var_gaus)
+
+var_exp <- Variogram(
+  m_exp, form = ~ x + y, robust = TRUE, resType = "normalized"
+)
+plot(var_exp)
